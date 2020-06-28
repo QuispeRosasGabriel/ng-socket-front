a7dec8ef861fc90dc31cfddb5ee7597d435322ad
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-06-27 19:08:37 -0500

    list usuarios component

--- src/app/components/lista-usuarios/lista-usuarios.component.ts
+++ src/app/components/lista-usuarios/lista-usuarios.component.ts
@@ -14,5 +14,6 @@ export class ListaUsuariosComponent implements OnInit {
 
   ngOnInit(): void {
     this.usuariosActivosObs = this._chatService.getUsuariosActivos();
+    this._chatService.emitirUsuariosActivos();
   }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -27,4 +27,8 @@ export class ChatService {
   getUsuariosActivos() {
     return this._webSocketService.listen('usuarios-activos');
   }
+
+  emitirUsuariosActivos() {
+    this._webSocketService.emit('obtener-usuarios');
+  }
 }
